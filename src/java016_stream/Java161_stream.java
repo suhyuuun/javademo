73ed1b5ecf8f9a0a4b8bda933f0b849bc0bf1e84
suhyun <suhyuuuuuun@gmail.com>
2022-10-20 15:44:34 +0900

    Update

--- src/java016_stream/Java161_stream.java
+++ src/java016_stream/Java161_stream.java
@@ -1,5 +1,22 @@
 package java016_stream;
 
-public class Java161_stream {
+import java.io.IOException;
+import java.io.InputStream;
 
+public class Java161_stream {
+	public static void main(String[] args) {
+		System.out.print("데이터 입력: ");
+		InputStream is = System.in; //console에 연결, 바이트 단위로 처리
+		
+		//.read() : 한 바이트를 읽어와 유니코드로 리턴한다.
+		try {
+			int line = is.read();
+			System.out.println(line);
+			System.out.println((char)line);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
 }
---
+++ src/java016_stream/Java162_stream.java
@@ -0,0 +1,23 @@
+package java016_stream;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Java162_stream {
+
+	public static void main(String[] args) {
+		System.out.print("데이터 입력:");
+		InputStream is = System.in;
+		int data;
+		
+		try {
+			while((data = is.read())!= 13) {
+				System.out.println((char)data);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}//end main()
+
+}//end class
