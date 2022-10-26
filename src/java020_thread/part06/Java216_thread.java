45c1e5a74e8bf824629e5f690b2aa84fdb9706b1
suhyun <suhyuuuuuun@gmail.com>
2022-10-26 14:59:42 +0900

    20221026 | ing

--- src/java020_thread/part06/Java216_thread.java
+++ src/java020_thread/part06/Java216_thread.java
@@ -3,7 +3,9 @@ package java020_thread.part06;
 public class Java216_thread {
 
 	public static void main(String[] args) {
-		Bank bank = new Bank();
+		AccountNumber aNumber = new AccountNumber();
+		
+		Bank bank = new Bank(aNumber);
 		Thread aPoint = new Thread(bank, "A");
 		aPoint.start();
 
---
+++ src/java020_thread/prob/VendingMachine.java
@@ -0,0 +1,5 @@
+package java020_thread.prob;
+
+public class VendingMachine {
+
+}
