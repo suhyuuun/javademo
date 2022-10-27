4850eda399c6eb5e98ba0b7b8a1d280aba60e71c
suhyun <suhyuuuuuun@gmail.com>
2022-10-27 14:40:45 +0900

    20221027 | Update

---
+++ src/ncs/answ/test10/Bonus.java
@@ -0,0 +1,6 @@
+package ncs.answ.test10;
+
+interface Bonus{
+	public void incentive(int pay);
+}
+
---
+++ src/ncs/answ/test13/AmountNotEnough.java
@@ -0,0 +1,9 @@
+package ncs.answ.test13;
+
+public class AmountNotEnough extends Exception {
+	
+	public AmountNotEnough(String message) {
+		super(message);
+	}
+}
+
